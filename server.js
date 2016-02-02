3012290a0319b52afc49675b0788e3e1d745c9d2
Ambika Castle <ambikacastle@gmail.com>
2016-02-01 20:21:20 -0800

    server to run express.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/booboo', function (req, res) {
+  res.send('Hello BooBoo!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
